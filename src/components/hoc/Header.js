998dbe4a21d83ae9311f57bce288fc6bd71f1509
TopFusion-dev <ramet.top@gmail.com>
2020-12-15 10:34:49 +0700

    [UPDATE] remove post, project hide

--- src/components/hoc/Header.js
+++ src/components/hoc/Header.js
@@ -14,8 +14,8 @@ const Header = () => {
         <div className="navbar-menu">
           <div className="navbar-end">
             <NavLink exact to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
-            <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
-            <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
+            {/*<NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>*/}
+            {/*<NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>*/}
             <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
             {/*<NavLink to="/counter" activeClassName="is-active" className="navbar-item">Counter App</NavLink>*/}
             <a className="navbar-item" href="https://github.com/ramet-top" target="_blank">GitHub
